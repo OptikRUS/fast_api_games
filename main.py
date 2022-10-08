2cbe133687275fe754f9d2f578f401d332eaba58
optikrus <optikrus61@gmail.com>
2022-10-08 19:39:11 +0300

    add responses

--- main.py
+++ main.py
@@ -2,42 +2,47 @@ import json
 
 from fastapi import FastAPI, Query
 
-from schemas import User, Game, Connection
+from schemas import User, UserResponse, Game, GameList, Connection
 from fixtures.utils import save_data
 
 app = FastAPI()
 
 
-@app.post("/users")
+@app.post("/users",
+          response_model=UserResponse,  # модель для Response, создает схему Response в доке
+          # response_model_exclude_unset=True, удаляет пустые параметры в Response
+          # response_model_include={"name", "age", "email"}, указание параметров из Response
+          )
 async def create_user(user: User):
     save_data('fixtures/users.json', user)
     return user
 
 
-@app.post("/games")
+@app.post("/games", response_model=Game,
+          response_model_exclude={"id"}  # удаляет параметры в Response, но не схему Response в доке
+          )
 async def create_game(game: Game):
     save_data('fixtures/games.json', game)
     return game
 
 
-@app.get("/games")
+@app.get("/games", response_model=GameList)
 async def get_games():
     with open('fixtures/games.json') as file:
         games_data = json.load(file)
-    return {'All games': games_data}
+        print(games_data)
+        all_games = GameList(games=games_data)
+    return all_games
 
 
-@app.get("/users/")
+@app.get("/users/{pk}", response_model=UserResponse)
 async def get_user(pk: int = Query(None, description="Get info about current user and info about all connected games")):
     with open('fixtures/users.json') as file:
         users_data = json.load(file)
-    if pk:
-        return {'User info': users_data[pk-1]}
-    return {'All users': users_data}
+        return UserResponse(**users_data[pk-1])
 
 
-@app.post("/connect")
+@app.post("/connect", response_model=Connection)
 async def connect_to_game(connection: Connection):
-    print(connection)
     save_data('fixtures/connections.json', connection)
-    return {'Connection': connection}
+    return connection
--- schemas.py
+++ schemas.py
@@ -4,14 +4,27 @@ from pydantic import BaseModel, Field
 class User(BaseModel):
     id: int
     name: str
-    age: int = Field(None, ge=0, le=100, description="User age must be more than 0 and less than 100")
-    email: str = Field(default='user.mail@mail.com',
-                       regex=r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
+    age: int = Field(..., ge=0, le=100, description="User age must be more than 0 and less than 100")
+    email: str = Field("example@mail.com", regex=r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
+
+
+class UserResponse(BaseModel):
+    name: str
+    age: int
+    email: str
 
 
 class Game(BaseModel):
     id: int
     name: str
+
+
+class GameResponse(BaseModel):
+    name: str
+
+
+class GameList(BaseModel):
+    games: list[GameResponse]
 
 
 class Connection(BaseModel):
